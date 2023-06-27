59c5383d3379663a25625705be2fe72dbd52d130
Sabrina Drashen <sdrashen@gmail.com>
2023-06-27 17:33:33 -0300

    style: finalize style nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from 'react'
-import { AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineMenu, AiOutlineClose  } from 'react-icons/ai'
 import SunIcon from '../assets/SunIcon.png'
 
 const Navbar = () => {
+
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0e192f] text-gray-300'>
       <div>
         <img src={SunIcon} alt="Ícone sol" style={{width: '50px'}} />
       </div>
+
       {/*Menu*/}
         <ul className='hidden md:flex'>
           <li>Home</li>
@@ -16,20 +21,23 @@ const Navbar = () => {
           <li>Work</li>
           <li>Contact</li>
         </ul>
+
       {/*Hamburguer*/}
-      <div className='md:hidden z-10'>
-      <AiOutlineMenu />
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <AiOutlineMenu /> : <AiOutlineClose />}
       </div>
+
       {/*Mobile menu */}
-      <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0e192f] flex flex-col justify-center items-center'>
+      <ul className={!nav ? 'hidden': 'absolute top-0 left-0 w-full h-screen bg-[#0e192f] flex flex-col justify-center items-center'}>
         <li className='py-6 text-4xl'>Home</li>
         <li className='py-6 text-4xl'>About</li>
         <li className='py-6 text-4xl'>Skills</li>
         <li className='py-6 text-4xl'>Work</li>
         <li className='py-6 text-4xl'>Contact</li>
       </ul>
+      
       {/*Social icons*/}
-      <div className='hidden'></div>
+      <div className=''></div>
     </div>
   )
 }
