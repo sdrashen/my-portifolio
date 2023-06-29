a622d81e515cea64f85814e4ba99d158765fe57a
Sabrina Drashen <sdrashen@gmail.com>
2023-06-29 11:39:30 -0300

    refactor: change info details

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,21 +7,19 @@ const About = () => {
         <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
           <div className='sm:text-right pb-8 pl-4'>
             <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
-              About
+              Sobre
             </p>
           </div>
           <div></div>
           </div>
           <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
             <div className='sm:text-right text-4xl font-bold'>
-              <p>Hi. I'm Sai Kumar, nice to meet you. Please take a look around.</p>
+              <p>Um pouco sobre mim...</p>
             </div>
             <div>
-              <p>I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?</p>  
+              <p className='leading-7 tracking-wider'>Sou natural do estado do Pará e há mais de 3 anos moro em Florianópolis. Minha primeira formação acadêmica é como Bacharel em Turismo. Atuei como intérprete, guia, monitora, conheci várias pessoas e lugares. Mas... Faltava aquele "click", sabe? Como sempre tive interesse na tecnologia, decidi investir
+              nessa área assim que tive a oportunidade. Decisão acertada! Gosto de criar, inovar, enfrentar desafios, sei trabalhar em equipe e acredito que neste novo campo profissional posso ir bem longe. Atualmente,
+              busco oportunidades de trabalho, aprendizado e crescimento no mercado tecnológico.</p>  
             </div>
           </div>
       </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
     <div name='home' className=' w-full h-screen bg-[#0e192f]'>
       {/*Container*/}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-lg text-pink-600 pb-6'>Oi, meu nome é</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Sabrina Drashen</h1>
+        <p className='text-lg text-pink-600 pb-6'>Olá, ilustre visitante. Meu nome é</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Sabrina Drashen.</h1>
         <h2 className='text-4xl pb-6 sm:text-7xl font-bold text-[#8892b0]'>Sou desenvolvedora Front End</h2>
         {/* <p className='text-[#8892b0] py-4 max-w-[700px]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga ratione eos hic reprehenderit. Consequatur, debitis minus quam suscipit nobis dolorem facilis, impedit esse excepturi quaerat numquam ipsum reiciendis ducimus quos.</p> */}
         <div>
