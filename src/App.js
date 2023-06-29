84f3e14809f2925ed933a8adeed885d28daffdf3
Sabrina Drashen <sdrashen@gmail.com>
2023-06-29 16:07:36 -0300

    feat: insert back to top button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import React, { useState, useEffect } from 'react'
+
 import About from "./components/About";
+import Button from "./components/Button";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
 
 function App() {
+  const [showButton, setShowButton] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+      setShowButton(scrollPosition > 660);
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+    window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+
   return (
     <div>
       <Navbar />
@@ -12,6 +37,7 @@ function App() {
       <About />
       <Skills />
       <Work />
+      {showButton && <Button onClick={scrollToTop}/>}
     </div>
   );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({onClick}) => {
+  return (
+    <button className='text-sm text-gray-300 button'>
+      Voltar ao topo
+    </button>
+  )
+}
+
+export default Button
