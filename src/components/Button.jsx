0dd5d4a2165855216dc388d4ef0df38af6352786
Sabrina Drashen <sdrashen@gmail.com>
2023-07-06 09:47:52 -0300

    fix: fix button back to top bug

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Button = ({onClick}) => {
   return (
-    <button className='text-sm text-gray-300 button'>
+    <button onClick={onClick} className='text-sm text-gray-300 button'>
       Voltar ao topo
     </button>
   )
