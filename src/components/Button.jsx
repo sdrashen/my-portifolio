f0fdfb53c7297f3b01a7a11d3b871cf323cebd41
Sabrina Drashen <sdrashen@gmail.com>
2023-07-09 16:30:33 -0300

    fix: button back to top language

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-
+import { useTranslation } from 'react-i18next'
 
 const Button = ({onClick}) => {
+
+  const { t } = useTranslation();
+
   return (
     <button onClick={onClick} className='text-sm text-gray-300 button'>
-      Voltar ao topo
+      {t("buttonToTop")}
     </button>
   )
 }
