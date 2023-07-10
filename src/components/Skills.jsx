fc0e987ccd2dda0665cecc70afba5b338a015d2b
Sabrina Drashen <sdrashen@gmail.com>
2023-07-10 10:13:00 -0300

    refactor: skills style

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -21,28 +21,28 @@ const Skills = () => {
           <p className='py-4'>{t("expText")}</p>
         </div>
         <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+          <div className='bg-[#204d91] shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
             <img className='w-20 mx-auto' src={ReactIcon} alt="React Ícone" />
             <p className='my-4'>React JS & Native</p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img src={StyledComponents} alt="Styled Component Ícone" className='w-20 mx-auto'/>
+          <div className='bg-[#204d91] shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img src={StyledComponents} alt="Styled Component Ícone" className='p-1 w-20 mx-auto'/>
             <p className='my-4'>Styled Components</p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img src={Tailwind} alt="Tailwind Ícone" className='w-20 mx-auto'/>
+          <div className='bg-[#204d91] shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img src={Tailwind} alt="Tailwind Ícone" className='p-1 w-20 mx-auto'/>
             <p className='my-4'>Tailwind</p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img src={Typescript} alt="Typescript Ícone" className='w-20 mx-auto'/>
+          <div className='bg-[#204d91] shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img src={Typescript} alt="Typescript Ícone" className='p-1 w-20 mx-auto'/>
             <p className='my-4'>Typescript</p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img src={Expo} alt="Expo Ícone" className='w-20 mx-auto'/>
+          <div className='bg-[#204d91] shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img src={Expo} alt="Expo Ícone" className='p-1 w-20 mx-auto'/>
             <p className='my-4'>Expo</p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-2'>
-            <img src={NextJS} alt="Next Js Ícone" className='w-20 h-20 mx-auto'/>
+          <div className='bg-[#204d91] shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-2'>
+            <img src={NextJS} alt="Next Js Ícone" className='p-1 w-20 h-20 mx-auto'/>
             <p className='my-4'>Next Js</p>
           </div>
         </div>
