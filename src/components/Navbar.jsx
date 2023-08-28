edc4519ecd2dc85061fc9b4c042f9c7b52f93d2b
Sabrina Drashen <sdrashen@gmail.com>
2023-08-28 14:49:33 -0300

    feat: add link to twitter

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { AiOutlineMenu, AiOutlineClose, AiFillLinkedin } from 'react-icons/ai'
+import { AiOutlineMenu, AiOutlineClose, AiFillLinkedin, AiFillTwitterSquare } from 'react-icons/ai'
 // import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import SunIcon from '../assets/SunIcon.png'
@@ -82,6 +82,12 @@ const Navbar = () => {
             <a className='flex justify-between items-center w-full text-gray-300' 
             href='https://www.canva.com/design/DAEKHPisvYQ/sJAlSVLk5EHW7d4LeUuVrQ/view?mode=prototype' target='_blank'>
               {t("resume")} <BsFillPersonLinesFill size={40}/>
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500'>
+            <a className='flex justify-between items-center w-full text-gray-300' 
+            href='https://twitter.com/sdrashen' target='_blank'>
+              Twitter <AiFillTwitterSquare size={40}/>
             </a>
           </li>
         </ul>
